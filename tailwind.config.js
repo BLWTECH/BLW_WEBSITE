c89a5bfe79e58d2b5e50cf4f2cbae6c32834908c
abdullah4tech <118620815+abdullah4tech@users.noreply.github.com>
2024-06-22 19:40:41 +0000

    feat: workinggit add .

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ export default {
   content: ["./index.html", "./pages/*.{html,js}"],
   theme: {
     screens: {
-      'tablet': '640px',
+      tablet: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'laptop': '1024px',
+      laptop: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'desktop': '1280px',
+      desktop: "1280px",
       // => @media (min-width: 1280px) { ... }
     },
     extend: {},

cf334ab6b03e4515fb431b7710cb1e5f4d53a084
abdullah4tech <118620815+abdullah4tech@users.noreply.github.com>
2024-06-24 21:01:59 +0000

    feat: Added routing features and render components

---
+++ pages/BlogPage.js
@@ -0,0 +1,7 @@
+export const BlogPage = () => {  // Use named export
+  return(`
+    <section class="m-10">
+      <h1 class="text-5xl">Blog Pages</h1>
+    </section>
+  `);
+}
---
+++ router/index.js
@@ -0,0 +1,38 @@
+import { HomePage } from '../pages/HomePage.js';
+import { PricingPage } from '../pages/PricingPage.js';
+import { BlogPage } from '../pages/BlogPage.js';
+import { PageNotFound } from '../pages/PageNotFound.js';
+
+const routes = {
+  '/': HomePage,
+  '/pricing': PricingPage,
+  '/blog': BlogPage,
+};
+
+const rootDiv = document.getElementById('app');
+
+const render = (route) => {
+  const page = routes[route] || PageNotFound;  // Fallback to HomePage if route is not found
+  rootDiv.innerHTML = page();
+};
+
+const onNavItemClick = (path) => {
+  window.history.pushState({}, path, window.location.origin + path);
+  render(path);
+};
+
+window.onpopstate = () => {
+  render(window.location.pathname);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('nav a')) {
+      e.preventDefault();
+      const path = e.target.getAttribute('href');
+      onNavItemClick(path);
+    }
+  });
+
+  render(window.location.pathname);  // Initial render based on current path
+});
